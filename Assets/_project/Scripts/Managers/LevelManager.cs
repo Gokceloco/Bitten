fd2f52d0c7f92737352791f4e933921ea5e75610
gokcegun <gokce@locomotive.games>
2025-10-06 15:42:04 +0300

    Initial commit

---
+++ Assets/_project/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public List<Level> levelPrefabs;
+    public int currentLevelNo;
+    private Level _currentLevel;
+    public void RestartLevelManager()
+    {
+        DeleteCurrentLevel();
+        CreateNewLevel();
+    }
+
+    private void CreateNewLevel()
+    {
+        _currentLevel = Instantiate(levelPrefabs[currentLevelNo - 1]);
+        _currentLevel.transform.position = Vector3.zero;
+    }
+
+    private void DeleteCurrentLevel()
+    {
+        if (_currentLevel)
+        {
+            Destroy(_currentLevel.gameObject);
+        }
+    }
+}
