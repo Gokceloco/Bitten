79b33b9240b2e102c367b90e77d8a8ccc9df3e36
gokcegun <gokce@locomotive.games>
2025-10-07 14:09:30 +0300

    DersSonu

--- Assets/_project/Scripts/Elements/PlayerMovement.cs
+++ Assets/_project/Scripts/Elements/PlayerMovement.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float walkSpeed;
+    public float runSpeed;
+
+    private Rigidbody _rb;
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        var direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector3.right;
+        }
+
+        var speed = walkSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+        }
+
+        MovePlayer(direction, speed);
+
+    }
+
+    void MovePlayer(Vector3 dir, float speed)
+    {
+        _rb.linearVelocity = dir.normalized * speed;
     }
 }
--- Assets/_project/Scripts/Managers/GameDirector.cs
+++ Assets/_project/Scripts/Managers/GameDirector.cs
@@ -6,6 +6,11 @@ public class GameDirector : MonoBehaviour
     public LevelManager levelManager;
     public Player player;
 
+    private void Start()
+    {
+        RestartLevel();
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
